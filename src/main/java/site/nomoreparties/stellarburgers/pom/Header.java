7157a0b29327b6a89eb9e05eeed4889c7fa9684c
ArtemSh13 <mozg1303@ya.ru>
2022-11-19 19:25:43 +0300

    Refactor header POM with MessageFormat

--- src/main/java/site/nomoreparties/stellarburgers/pom/Header.java
+++ src/main/java/site/nomoreparties/stellarburgers/pom/Header.java
@@ -3,30 +3,30 @@ package site.nomoreparties.stellarburgers.pom;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.text.MessageFormat;
+
 public class Header {
 
     private WebDriver driver;
 
-    private By headerLinks = By.className("AppHeader_header__linkText__3q_va ml-2");
-
-    private int constructorButtonIndex = 0;
-
-    private int ordersFeedButtonIndex = 1;
-
     private By stellarBurgersLink = By.className("AppHeader_header__logo__2D0X2");
 
-    private int profileLinkIndex = 2;
+    private String headerElementsXpath = ".//*[@class='AppHeader_header__linkText__3q_va ml-2' and text()={0}]";
+
+    private By headerConstructorButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Конструктор"));
+    private By headerFeedButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Лента Заказов'"));
+    private By headerProfileButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Личный Кабинет'"));
 
     public Header(WebDriver driver) {
         this.driver = driver;
     }
 
     public void clickConstructorButton() {
-        driver.findElements(headerLinks).get(constructorButtonIndex).click();
+        driver.findElement(headerConstructorButton).click();
     }
 
     public void clickOrdersFeedButton() {
-        driver.findElements(headerLinks).get(ordersFeedButtonIndex).click();
+        driver.findElement(headerFeedButton).click();
     }
 
     public void clickStellarBurgersLink() {
@@ -34,6 +34,6 @@ public class Header {
     }
 
     public void clickPersonalAreaLink() {
-        driver.findElements(headerLinks).get(profileLinkIndex).click();
+        driver.findElement(headerProfileButton).click();
     }
 }
