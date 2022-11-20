79071bc6e4c3a73918f2793161667c673eff4d0b
ArtemSh13 <mozg1303@ya.ru>
2022-11-20 20:17:24 +0300

    Replace MessageFormat with simple concatenation

--- src/main/java/site/nomoreparties/stellarburgers/pom/Header.java
+++ src/main/java/site/nomoreparties/stellarburgers/pom/Header.java
@@ -3,19 +3,16 @@ package site.nomoreparties.stellarburgers.pom;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import java.text.MessageFormat;
-
 public class Header {
 
     private WebDriver driver;
 
     private By stellarBurgersLink = By.className("AppHeader_header__logo__2D0X2");
 
-    private String headerElementsXpath = ".//*[@class='AppHeader_header__linkText__3q_va ml-2' and text()={0}]";
-
-    private By headerConstructorButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Конструктор"));
-    private By headerFeedButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Лента Заказов'"));
-    private By headerProfileButton = By.xpath(MessageFormat.format(headerElementsXpath,"'Личный Кабинет'"));
+    private String headerElementsXpath = ".//*[@class='AppHeader_header__linkText__3q_va ml-2' and text()=";
+    private By headerConstructorButton = By.xpath(headerElementsXpath + "'Конструктор']");
+    private By headerFeedButton = By.xpath(headerElementsXpath + "'Лента Заказов']");
+    private By headerProfileButton = By.xpath(headerElementsXpath + "'Личный Кабинет']");
 
     public Header(WebDriver driver) {
         this.driver = driver;
